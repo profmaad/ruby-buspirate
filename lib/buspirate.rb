5877f56a32a9c32d27bf1e32414c5c4d7d625b42
Marcos Piccinini <x@nofxx.com>
2012-04-12 04:47:46 -0300

    got it to working with buspirate v4 clean constants add clean_buffer method, buspirate responds with lots of BBIO1! bug?

--- lib/buspirate.rb
+++ lib/buspirate.rb
@@ -1,52 +1,12 @@
-require 'rubygems'
+#
+# Bus Pirarrte!
+#
+#http://dangerousprototypes.com/2009/10/09/bus-pirate-raw-bitbang-mode/
+#require 'rubygems'
 require 'serialport'
+require 'bus_pirate/constants'
 
 class BusPirate
-  class Mode
-    RESET = 0
-    SPI = 1
-    I2C = 2
-    UART = 3
-    ONEWIRE = 4
-    RAWWIRE = 5
-  end
-  class PinMode
-    OUTPUT = 0
-    INPUT = 1
-  end
-  class UART
-    PIN_OUTPUT_HIZ = 0b00000000
-    PIN_OUTPUT_33V = 0b00010000
-    FORMAT_8N = 0b00000000
-    FORMAT_8E = 0b00000100
-    FORMAT_80 = 0b00001000
-    FORMAT_9N = 0b00001100
-    STOPBITS_1 = 0b00000000
-    STOPBITS_2 = 0b00000010
-    IDLE_POLARITY_1 = 0b00000000
-    IDLE_POLARITY_0 = 0b00000001
-  end
-  class SPI
-    SPEED_30KHZ  = 0b00000000
-    SPEED_125KHZ = 0b00000001
-    SPEED_250KHZ = 0b00000010
-    SPEED_1MHZ   = 0b00000011
-    SPEED_2MHZ   = 0b00000100
-    SPEED_2_6MHZ = 0b00000101
-    SPEED_4MHZ   = 0b00000110
-    SPEED_8MHZ   = 0b00000111
-    PIN_OUTPUT_HIZ = 0b00000000
-    PIN_OUTPUT_33V = 0b00001000
-    CLOCK_IDLE_LOW = 0b00000000
-    CLOCK_IDLE_HIGH = 0b00000100
-    CLOCK_EDGE_IDLE_TO_ACTIVE = 0b00000000
-    CLOCK_EDGE_ACTIVE_TO_IDLE = 0b00000010
-    SAMPLE_TIME_MIDDLE = 0b00000000
-    SAMPLE_TIME_END = 0b00000001
-  end
-
-  FOSC = 32000000
-  UART_CLOCK_DIVIDER = 2
 
   attr_reader :port
 
@@ -68,20 +28,7 @@ class BusPirate
     @port.putc '#'
   end
 
-  def enter_bitbang
-    (0..25).each do |i|
-      @port.putc 0x00
 
-      selectResult = IO.select([@port],[],[],0.2)
-      if selectResult
-        result = check_for_ack("BBIO1")
-
-        return true if result
-      end
-    end
-
-    return false
-  end
 
   def exit_bitbang
     @port.putc 0x0f
@@ -103,6 +50,31 @@ class BusPirate
     end
 
     return true
+  end
+
+  def clear_buffer
+    selectResult = IO.select([@port],[],[],0.2)
+    if selectResult
+      @port.readbyte
+      clear_buffer
+    end
+  end
+
+  #
+  # Start binary connection
+  #
+  def enter_bitbang
+    loop do
+      @port.putc 0b0000000
+
+      selectResult = IO.select([@port],[],[],0.2)
+      if selectResult && check_for_ack("BBIO1")
+        clear_buffer
+        return true
+      end
+    end
+
+    return false
   end
 
   def switch_mode(mode)
